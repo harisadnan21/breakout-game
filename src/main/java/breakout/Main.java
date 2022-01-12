dbc98ca0dda977cf738bd7a6463c2de8391054a3
ha109 <haris.adnan@duke.edu>
2022-01-11 23:01:27 -0500

    set up game

--- src/main/java/breakout/Main.java
+++ src/main/java/breakout/Main.java
@@ -1,5 +1,10 @@
 package breakout;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.scene.paint.Paint;
+import javafx.stage.Stage;
+import javafx.util.Duration;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -15,8 +20,14 @@ import javafx.scene.shape.Circle;
  */
 public class Main extends Application {
     // useful names for constant values used
-    public static final String TITLE = "Example JavaFX Animation";
-    public static final int SIZE = 750;
+    public static final String TITLE = "Breakout Project";
+    public static final int SIZE = 400;
+    public static final Paint BACKGROUND = Color.PEACHPUFF;
+    public static final int FRAMES_PER_SECOND = 60;
+    public static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
+
+    // instance variables
+    private runClass myGame;
 
 
     /**
@@ -24,16 +35,17 @@ public class Main extends Application {
      */
     @Override
     public void start (Stage stage) {
-        Circle shape = new Circle(SIZE / 2 , SIZE / 2, 14);
-        shape.setFill(Color.LIGHTSTEELBLUE);
+        myGame = new runClass();
 
-        Group root = new Group();
-        root.getChildren().add(shape);
-
-        Scene scene = new Scene(root, SIZE, SIZE, Color.DARKBLUE);
+        // attach scene to the stage and display it
+        Scene scene = myGame.setupGame(SIZE, SIZE, BACKGROUND);
         stage.setScene(scene);
-
         stage.setTitle(TITLE);
         stage.show();
+        // attach "game loop" to timeline to play it (basically just calling step() method repeatedly forever)
+        Timeline animation = new Timeline();
+        animation.setCycleCount(Timeline.INDEFINITE);
+        animation.getKeyFrames().add(new KeyFrame(Duration.seconds(SECOND_DELAY), e -> myGame.step(SECOND_DELAY)));
+        animation.play();
     }
 }
---
+++ src/main/java/breakout/runClass.java
@@ -0,0 +1,177 @@
+package breakout;
+
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
+
+/**
+ * The class that setups the breakout window
+ *
+ * @author Haris Adnan
+ */
+
+public class runClass {
+    public static final String RESOURCE_PATH = "/";
+    public static final String BOUNCER_IMAGE = RESOURCE_PATH + "duke-seal-logo.png";
+    public static final int BOUNCER_SIZE = 40;
+    public static final int BOUNCER_SPEED = 50;
+    public static final Paint MOVER_COLOR = Color.PLUM;
+    public static final int MOVER_SIZE = 50;
+    public static final int MOVER_SPEED = 8;
+    public static final Paint GROWER_COLOR = Color.BISQUE;
+    public static final double GROWER_RATE = 1.1;
+    public static final int GROWER_SIZE = 50;
+    public static final Paint HIGHLIGHT = Color.OLIVEDRAB;
+    public static final Paint PADDLE_COLOR = Color.BLACK;
+    public static final int PADDLE_SIZE = 70;
+    public static final int PADDLE_SPEED= 20;
+
+    private ImageView myBouncer;
+    private Rectangle myMover;
+    private Rectangle myGrower;
+    private Rectangle myPaddle;
+    private  int directionX = 1;
+    private  int directionY = 3;
+
+    public Scene setupGame (int width, int height, Paint background) {
+        // create one top level collection to organize the things in the scene
+        Group root = new Group();
+        // make an image based on a file "resource" found using Java's classpath
+        Image image = new Image(getClass().getResourceAsStream(BOUNCER_IMAGE));
+        myBouncer = new ImageView(image);
+        myBouncer.setFitWidth(BOUNCER_SIZE);
+        myBouncer.setFitHeight(BOUNCER_SIZE);
+        // x and y represent the top left corner, so center it in window by offsetting by the width nd height
+
+        myBouncer.setX(300 / 2 - myBouncer.getBoundsInLocal().getWidth() / 2);
+        myBouncer.setY(300 / 2 - myBouncer.getBoundsInLocal().getHeight() / 2);
+        // make some shapes and set their properties
+        myMover = new Rectangle(300 - MOVER_SIZE / 2, height / 2 - 100, MOVER_SIZE, MOVER_SIZE);
+        myMover.setFill(MOVER_COLOR);
+        myGrower = new Rectangle(100 - GROWER_SIZE / 2, height / 2 + 50, GROWER_SIZE, GROWER_SIZE);
+        myGrower.setFill(GROWER_COLOR);
+        // ADDING PADDLE
+        myPaddle = new Rectangle(width/ 2 - PADDLE_SIZE/2 , height - 50, PADDLE_SIZE, PADDLE_SIZE /2 );
+        myPaddle.setFill(PADDLE_COLOR);
+
+        // order added to the group is the order in which they are drawn
+        root.getChildren().add(myBouncer);
+        root.getChildren().add(myMover);
+        root.getChildren().add(myGrower);
+        root.getChildren().add(myPaddle);
+
+        // create a place to see the shapes
+        Scene scene = new Scene(root, width, height, background);
+        // respond to input
+        scene.setOnKeyPressed(e -> handleKeyInput(e.getCode()));
+        scene.setOnMouseClicked(e -> handleMouseInput(e.getX(), e.getY()));
+        return scene;
+    }
+    public void step (double elapsedTime) {
+        // update "actors" attributes
+        // bouncer moves at a "constant" rate now matter how many frames are drawn per second
+
+        myBouncer.setX(myBouncer.getX() + BOUNCER_SPEED * elapsedTime * directionX);
+        myBouncer.setY(myBouncer.getY() + BOUNCER_SPEED * elapsedTime * directionY);
+        // shapes move at a "variable" rate, faster for higher frame rate
+        //myMover.setRotate(myMover.getRotate() - 1);
+        //myGrower.setRotate(myGrower.getRotate() + 1);
+        System.out.println(myBouncer.getX());
+        System.out.println(directionX);
+        if (myBouncer.getX()<= 0 || myBouncer.getX() >= Main.SIZE - myBouncer.getFitWidth()){
+            directionX = -1 * directionX;
+
+        }
+        if (myBouncer.getY()<= 0 || myBouncer.getY() >= Main.SIZE - myBouncer.getFitHeight()){
+            directionY = -1 * directionY;
+        }
+
+        // check for collisions
+        if (isIntersecting(myBouncer, myMover)) {
+            // myMover.setFill(HIGHLIGHT);
+
+            if(myBouncer.getX() >= myMover.getX()|| myBouncer.getX() <= myMover.getX() + MOVER_SIZE){
+                directionX = -1 * directionX;
+            }
+            else if(myBouncer.getY() >= myMover.getY() || myBouncer.getY() <= myMover.getY() + MOVER_SIZE){
+                directionY = -1 * directionY;
+            }
+        }
+//        else {
+//            myMover.setFill(MOVER_COLOR);
+//        }
+        if (isIntersecting(myBouncer, myGrower)) {
+//            myGrower.setScaleX(1);
+//            myGrower.setScaleY(1);
+            if(myBouncer.getX() >= myGrower.getX()|| myBouncer.getX() <= myGrower.getX() + MOVER_SIZE){
+                directionX = -1 * directionX;
+            }
+            else if(myBouncer.getY() >= myGrower.getY() || myBouncer.getY() <= myGrower.getY() + MOVER_SIZE){
+                directionY = -1 * directionY;
+            }
+
+
+
+        }
+        if (isIntersecting(myBouncer, myPaddle)) {
+//            myGrower.setScaleX(1);
+//            myGrower.setScaleY(1);
+            directionY = -1 * directionY;
+
+        }
+
+
+    }
+
+    // What to do each time a key is pressed
+    private void handleKeyInput (KeyCode code) {
+        // NOTE new Java syntax that some prefer (but watch out for the many special cases!)
+        //   https://blog.jetbrains.com/idea/2019/02/java-12-and-intellij-idea/
+        switch (code) {
+            case RIGHT -> myPaddle.setX(myPaddle.getX() + PADDLE_SPEED);
+            case LEFT -> myPaddle.setX(myPaddle.getX() - PADDLE_SPEED);
+            case UP -> myPaddle.setY(myPaddle.getY() - PADDLE_SPEED);
+            case DOWN -> myPaddle.setY(myPaddle.getY() + PADDLE_SPEED);
+        }
+        // TYPICAL way to do it, definitely more readable for longer actions
+//        if (code == KeyCode.RIGHT) {
+//            myMover.setX(myMover.getX() + MOVER_SPEED);
+//        }
+//        else if (code == KeyCode.LEFT) {
+//            myMover.setX(myMover.getX() - MOVER_SPEED);
+//        }
+//        else if (code == KeyCode.UP) {
+//            myMover.setY(myMover.getY() - MOVER_SPEED);
+//        }
+//        else if (code == KeyCode.DOWN) {
+//            myMover.setY(myMover.getY() + MOVER_SPEED);
+//        }
+    }
+
+    // What to do each time a key is pressed
+    private void handleMouseInput (double x, double y) {
+        if (myGrower.contains(x, y)) {
+            myGrower.setScaleX(myGrower.getScaleX() * GROWER_RATE);
+            myGrower.setScaleY(myGrower.getScaleY() * GROWER_RATE);
+        }
+    }
+
+    // Name for a potentially complex comparison to make code more readable
+    private boolean isIntersecting (ImageView a, Rectangle b) {
+        // with images can only check bounding box (as it is calculated in container with other objects)
+        return b.getBoundsInParent().intersects(a.getBoundsInParent());
+        // with shapes, can check precisely (in this case, it is easy because the image is circular)
+//        Shape bouncerBounds = new Circle(a.getX() + a.getFitWidth() / 2,
+//                                       a.getY() + a.getFitHeight() / 2,
+//                                       a.getFitWidth() / 2 - BOUNCER_SIZE / 20);
+//        return ! Shape.intersect(bouncerBounds, b).getBoundsInLocal().isEmpty();
+    }
+
+}
